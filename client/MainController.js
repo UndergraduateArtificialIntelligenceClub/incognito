81ccbef8d90dc7f32755cc30293254a56832abdb
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-09-27 16:09:23 -0600

    name email uniqueid

--- client/MainController.js
+++ client/MainController.js
@@ -1,4 +1,7 @@
 app.controller("MainController", ["$scope", function($scope) {
-    $scope.foo = "foo";
-    $scope.bar = "bar";
+    $scope.info_box = {
+        name: "Timmy",
+        email: "timmy12@ualberta.ca",
+        uniqueId: "7$#@i3f79%%2132"
+    };
 }]);
